077d0e8ac4b8f6c206eab8cfeacf6c9fb69aa9f5
Vayel <vincent.lefoulon@free.fr>
2014-05-10 06:26:38 -0700

    Add tiles

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+function addEvent(element, event, func) {
+	if (element.addEventListener) element.addEventListener(event, func, false);
+	else element.attachEvent('on' + event, func);
+}
+
 var map = Map.create({
 	container: {
 		div: document.querySelector('#map-container')
@@ -6,11 +11,6 @@ var map = Map.create({
 		div: document.querySelector('#map-thumbnail')
 	},
 	cb: function() { // After the map was created
-		$('#show-map-btn').click(function(e) {
-			var data = document.querySelector('#show-map-btn').dataset;
-			map.center(data.left, data.top);
-		});	
-		
 		map.addLink({
 			nw: [49.5, 79.5], // North-West: (49.5%, 79.5%)
 			se: [59, 81.5], // South-East: (59%, 81.5%)
